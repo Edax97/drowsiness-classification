abcfd3fb1fc16d0c1572ce9d4e581b71e54db71b
Edwin Campos <ecamposc97@gmail.com>
2026-02-19 19:06:32 -0500

    desktop setup

--- host/setup.sh
+++ host/setup.sh
@@ -3,7 +3,5 @@
 sudo cp start_drowsiness.sh ../test/start_test.sh /usr/local/bin/
 sudo cp .env /usr/local/etc/
 
-sudo cp drowsiness.service test.service "$HOME/.config/systemd/user/"
-systemctl --user daemon-reload
-systemctl --user enable drowsiness.service
-systemctl --user restart drowsiness.service
+sudo cp test.service "$HOME/.config/systemd/user/"
+cp drowsiness.desktop "$HOME/.config/autostart/"
