e4ea3a501e55a5265dea8917d05b339142d862e6
Edwin Campos <ecamposc97@gmail.com>
2026-02-19 20:03:17 -0500

    delete test service

--- host/init_drowsiness.sh
+++ host/init_drowsiness.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 source /usr/local/etc/.env
-export DROWSY_DET_FILE, AWAKE_DET_FILE, DEVICE
+export DROWSY_DET_FILE AWAKE_DET_FILE DEVICE
 cd /home/pi/app/inference || exit
 /home/pi/.venv/bin/python detect_drowsiness.py
