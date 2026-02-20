fa3e794108f56d10d6f0f9292d9b0327b93b5c54
Edwin Campos <ecamposc97@gmail.com>
2026-02-19 19:33:22 -0500

    desktop setup

--- host/setup.sh
+++ host/setup.sh
@@ -3,8 +3,11 @@ mkdir -p "$HOME/app"
 sudo cp -r ../test "$HOME/app/test"
 sudo cp -r ../inference "$HOME/app/inference"
 
+sudo chmod +x init_drowsiness.sh init_test.sh
 sudo cp init_drowsiness.sh init_test.sh /usr/local/bin/
 sudo cp .env /usr/local/etc/
+
+sudo chmod 644 test_drowsy.service drowsiness.desktop
 
 sudo cp test_drowsy.service "$HOME/.config/systemd/user/"
 cp drowsiness.desktop "$HOME/.config/autostart/"
