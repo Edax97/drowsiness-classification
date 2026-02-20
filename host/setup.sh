1d5abbfa8b7c5ac143f05de3a2e71fe317367d15
Edwin Campos <ecamposc97@gmail.com>
2026-02-19 19:58:32 -0500

    delete test service

--- host/setup.sh
+++ host/setup.sh
@@ -4,7 +4,7 @@ cp -r ../test "$HOME/app/test"
 cp -r ../inference "$HOME/app/inference"
 
 sudo cp init_drowsiness.sh /usr/local/bin/
-sudo chmod +x /usr/local/bin/*.sh
 sudo cp .env /usr/local/etc/
+sudo chmod +x /usr/local/bin/*.sh
 
 cp drowsiness.desktop "$HOME/.config/autostart/"
